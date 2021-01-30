f40373ac93bc5bf69f5aa7cc7c4aef7ab5dc5ab4
R-Tsukada <r.tsukada14@gmail.com>
2021-01-30 13:31:44 +0900

    Sign upに郵便番号・住所・自己紹介文を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,26 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  # deviseコントローラーにストロングパラメータを追加する
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # ログイン後の遷移
   def after_sign_in_path_for(_resource)
     books_path
   end
-
+  # ログアウト後の遷移
   def after_sign_out_path_for(_resource)
     new_user_session_path
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    #よくわからない
+    added_attrs = [:username, :email, :password, :postal_code,:address,:description,:password_confirmation, :remember_me]
+    # サインアップ時にemailのストロングパラメータを追加
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    # アカウント編集の時にnameとprofileのストロングパラメータを追加
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
 end
