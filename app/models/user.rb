4cc7fc6b08315d85c3e7de44dfdbac83f64c412a
R-Tsukada <r.tsukada14@gmail.com>
2021-03-11 15:55:41 +0900

    userモデルにfollowingの関連づけとメソッドを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name: "Relationship",
                                   foreign_key: "follower_id",
                                   dependent: :destroy
+  has_many :followings, through: :active_relationships, source: :followed
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
@@ -19,5 +20,17 @@ class User < ApplicationRecord
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
     end
+  end
+
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow(other_user)
+    active.relationships.find_by(followed_id, other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
   end
 end
