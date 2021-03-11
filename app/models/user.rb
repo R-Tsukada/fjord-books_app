82ffc2ab52d5002f81056de7361150e2f60b6036
R-Tsukada <r.tsukada14@gmail.com>
2021-03-11 15:37:50 +0900

    active_relationshipsに対して1対多の関連づけを実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: %i[github]
