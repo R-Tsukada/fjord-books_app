b7541ac83b31f60aba5a41a976de259d79b4a20a
R-Tsukada <r.tsukada14@gmail.com>
2021-03-15 10:05:09 +0900

    userとRelationshipにアソシエーションを指定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_many :active_relationships, class_name: "Relationship",
-                                  foreign_key: "follower_id",
-                                  dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :followings, through: :active_relationships, source: :followed
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
