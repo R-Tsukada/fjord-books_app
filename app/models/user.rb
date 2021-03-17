c1dd76ea2bd6ac680755c38391dcf590335b695c
R-Tsukada <r.tsukada14@gmail.com>
2021-03-17 14:38:31 +0900

    rubocop対応

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: %i[github]
+
   has_many :active_relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy, inverse_of: :follower
   has_many :following, through: :active_relationships, source: :following
   has_many :passive_relationships, class_name: 'Relationship', foreign_key: 'following_id', dependent: :destroy, inverse_of: :following
