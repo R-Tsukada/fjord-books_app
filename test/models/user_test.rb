043e0d4bf70d10e16fb271dab6b9c274900698bb
R-Tsukada <r.tsukada14@gmail.com>
2021-04-12 10:31:05 +0900

    ユーザーテストを作成

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,9 +1,40 @@
-# frozen_string_literal: true
-
-require 'test_helper'
+require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#name_or_email" do
+    user = User.new(email: 'foo@example.com', name: '')
+    assert_equal 'foo@example.com', user.name_or_email
+
+    user.name = "foo bar"
+    assert_equal 'foo bar', user.name_or_email
+  end
+
+  test "#following?" do
+    alice = users(:alice)
+    bob = users(:bob)
+
+    alice.follow(bob)
+    assert alice.following?(bob)
+    assert bob.followed_by?(alice)
+  end
+
+  test "#follow" do
+    carol = users(:carol)
+    dave = users(:dave)
+
+    assert_not carol.following?(dave)
+    carol.following?(dave)
+    carol.follow(dave)
+    assert carol.following?(dave)
+  end
+
+  test "#unfollow" do
+    ellen = users(:ellen)
+    frank = users(:frank)
+
+    ellen.follow(frank)
+    assert ellen.following?(frank)
+    ellen.unfollow(frank)
+    assert_not frank.followed_by?(ellen)
+  end
 end
