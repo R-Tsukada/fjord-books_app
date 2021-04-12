58d41bd5b1fc42150e3225eccd943e76f8433960
R-Tsukada <r.tsukada14@gmail.com>
2021-04-12 10:35:34 +0900

    日報のモデルテストを作成

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,17 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#created_on" do
+    report = reports(:alice_report)
+    assert_equal report.created_at.to_date, report.created_on
+  end
+
+  test "#editable" do
+    bob = users(:bob)
+    bob_report = reports(:bob_report)
+    assert bob_report.editable?(bob)
+
+    carol = users(:carol)
+    assert_not bob_report.editable?(carol)
+  end
 end
