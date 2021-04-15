c8bb267cae70faea6d5af5b40444b3e3ae6b422b
R-Tsukada <r.tsukada14@gmail.com>
2021-04-15 20:10:07 +0900

    rubocop対応

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,12 +3,12 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  test "#created_on" do
+  test '#created_on' do
     report = reports(:alice_report)
     assert_equal report.created_at.to_date, report.created_on
   end
 
-  test "#editable" do
+  test '#editable' do
     bob = users(:bob)
     bob_report = reports(:bob_report)
     assert bob_report.editable?(bob)
